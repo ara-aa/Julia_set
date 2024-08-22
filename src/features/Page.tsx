440aeebb80066a84c47fddd42064f7e2a8f23538
ara-aa <miho.araki.csj@gmail.com>
2024-08-22 23:49:17 +0900

    style: fix layout / fix: schema

--- src/features/Page.tsx
+++ src/features/Page.tsx
@@ -1,4 +1,9 @@
 import { Button, Form, FormInput, Label, Layout, Spinner } from '@/components'
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable'
 import { action } from './action'
 import { COMP_CONST, MAX_X, MAX_Y, MIN_X, MIN_Y } from './julia.constants'
 
@@ -7,38 +12,53 @@ const Page = () => {
 
   return (
     <Layout>
-      <Form {...juliaForm}>
-        <form
-          onSubmit={juliaForm.handleSubmit(onSubmit)}
-          className='grid gap-7'
-        >
-          <FormInput form={juliaForm} name='min_x' label={MIN_X} />
-          <FormInput form={juliaForm} name='max_x' label={MAX_X} />
-          <FormInput form={juliaForm} name='min_y' label={MIN_Y} />
-          <FormInput form={juliaForm} name='max_y' label={MAX_Y} />
-          <Label>{COMP_CONST}</Label>
-          <div className='flex'>
-            <FormInput
-              form={juliaForm}
-              name='comp_const_a'
-              label={`${COMP_CONST}_a`}
-            />
-            +
-            <FormInput
-              form={juliaForm}
-              name='comp_const_b'
-              label={`${COMP_CONST}_b`}
-            />
-            i
-          </div>
-          <Button type='submit'>Submit</Button>
-        </form>
-      </Form>
+      <ResizablePanelGroup direction='horizontal'>
+        <ResizablePanel>
+          <Form {...juliaForm}>
+            <form
+              onSubmit={juliaForm.handleSubmit(onSubmit)}
+              className='grid gap-3 ml-2'
+            >
+              <FormInput form={juliaForm} name='min_x' label={MIN_X} />
+              <FormInput form={juliaForm} name='max_x' label={MAX_X} />
+              <FormInput form={juliaForm} name='min_y' label={MIN_Y} />
+              <FormInput form={juliaForm} name='max_y' label={MAX_Y} />
 
-      {png && (
-        <div className='julia'>{png && <img alt='julia_set' src={png} />}</div>
-      )}
+              <Label className='text-lg'>{COMP_CONST}</Label>
+              <div className='flex items-end gap-2'>
+                <FormInput
+                  form={juliaForm}
+                  name='comp_const_a'
+                  label={`${COMP_CONST}_a`}
+                />
+                <p className='font-semibold'>+</p>
+                <FormInput
+                  form={juliaForm}
+                  name='comp_const_b'
+                  label={`${COMP_CONST}_b`}
+                />
+                <p className='font-semibold'>i</p>
+              </div>
 
+              <div className='mt-4'>
+                <Button type='submit'>Submit</Button>
+              </div>
+            </form>
+          </Form>
+        </ResizablePanel>
+
+        <ResizableHandle />
+
+        {png && (
+          <>
+            <ResizablePanel>
+              <div className='julia'>
+                {png && <img alt='julia_set' src={png} />}
+              </div>
+            </ResizablePanel>
+          </>
+        )}
+      </ResizablePanelGroup>
       {isLoading && <Spinner size='large' />}
     </Layout>
   )
--- src/features/juliaSet.validation.ts
+++ src/features/juliaSet.validation.ts
@@ -1,16 +1,80 @@
+import * as math from 'mathjs'
 import * as yup from 'yup'
 import { COMP_CONST, MAX_X, MAX_Y, MIN_X, MIN_Y } from './julia.constants'
 
 const REQUIRED = '必須入力項目です。'
+const NUM_REGEX = /^-?(\d|\d+|\d+\.\d+)?$/
 
 export const schema = yup.object().shape({
-  min_x: yup.number().label(MIN_X).required(REQUIRED),
-  max_x: yup.number().label(MAX_X).required(REQUIRED),
-  min_y: yup.number().label(MIN_Y).required(REQUIRED),
-  max_y: yup.number().label(MAX_Y).required(REQUIRED),
-  comp_const_a: yup.number().label(`${COMP_CONST}_a`).required(REQUIRED),
-  comp_const_b: yup.number().label(`${COMP_CONST}_b`).required(REQUIRED),
-  // 実数(半角スペース)虚数i の形で入力してください。
+  min_x: yup
+    .string()
+    .label(MIN_X)
+    .required(REQUIRED)
+    .matches(NUM_REGEX, '数値を入力してください'),
+  max_x: yup
+    .string()
+    .label(MAX_X)
+    .required(REQUIRED)
+    .matches(NUM_REGEX, '数値を入力してください'),
+  min_y: yup
+    .string()
+    .label(MIN_Y)
+    .required(REQUIRED)
+    .matches(NUM_REGEX, '数値を入力してください'),
+  max_y: yup
+    .string()
+    .label(MAX_Y)
+    .required(REQUIRED)
+    .matches(NUM_REGEX, '数値を入力してください'),
+  comp_const_a: yup
+    .string()
+    .label(`${COMP_CONST}_a`)
+    .required(REQUIRED)
+    .matches(NUM_REGEX, '数値を入力してください')
+    .test(
+      'custom',
+      '複素定数が2より大きいため描画できません。',
+      function (comp_a) {
+        if (
+          math.larger(
+            math.abs(Number(comp_a) + -Number(this.parent.comp_const_b)),
+            2,
+          )
+        ) {
+          return false
+        }
+        return true
+      },
+    ),
+  comp_const_b: yup
+    .string()
+    .label(`${COMP_CONST}_b`)
+    .required(REQUIRED)
+    .matches(NUM_REGEX, '数値を入力してください')
+    .test(
+      'custom',
+      '複素定数が2より大きいため描画できません。',
+      function (comp_b) {
+        if (
+          math.larger(
+            math.abs(Number(this.parent.comp_const_a) + -Number(comp_b)),
+            2,
+          )
+        ) {
+          return false
+        }
+        return true
+      },
+    ),
 })
 
 export type SchemaType = yup.InferType<typeof schema>
+
+export const defaultValues = {
+  min_x: '',
+  max_x: '',
+  min_y: '',
+  max_y: '',
+  comp_const_a: '',
+  comp_const_b: '',
+}
